a8e5820b62e8ca21c4b1080bd893b93a12476d31
조영현 <cyh@joyeonghyeon-ui-MacBookAir.local>
2023-06-27 16:09:49 +0900

    first commit

---
+++ src/main/java/com/basak/dalcom/DalcomApplication.java
@@ -0,0 +1,13 @@
+package com.basak.dalcom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DalcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DalcomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/basak/dalcom/DalcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basak.dalcom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DalcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
