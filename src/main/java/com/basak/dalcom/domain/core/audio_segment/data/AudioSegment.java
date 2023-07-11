61d212fe0eac890b4878ccbb759a86714c037130
pcjs156 <qkrdbqls1001@gmail.com>
2023-07-11 21:04:57 +0900

    refactor: Presentation, Speech, AudioSegment 위치 수정

--- src/main/java/com/basak/dalcom/domain/core/audio_segment/data/AudioSegment.java
+++ src/main/java/com/basak/dalcom/domain/core/audio_segment/data/AudioSegment.java
@@ -1,13 +1,22 @@
-package com.basak.dalcom.domain.core;
+package com.basak.dalcom.domain.core.audio_segment.data;
 
 import com.basak.dalcom.domain.common.BaseEntity;
-
-import javax.persistence.*;
+import com.basak.dalcom.domain.core.speech.data.Speech;
 import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Transient;
 
 @Entity
 @Table(name = "audio_segment")
 public class AudioSegment extends BaseEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
--- src/main/java/com/basak/dalcom/domain/core/speech/data/Speech.java
+++ src/main/java/com/basak/dalcom/domain/core/speech/data/Speech.java
@@ -1,13 +1,26 @@
-package com.basak.dalcom.domain.core;
+package com.basak.dalcom.domain.core.speech.data;
 
 import com.basak.dalcom.domain.common.BaseEntity;
-
-import javax.persistence.*;
+import com.basak.dalcom.domain.core.audio_segment.data.AudioSegment;
+import com.basak.dalcom.domain.core.presentation.data.Presentation;
 import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import lombok.Getter;
 
+@Getter
 @Entity
 @Table(name = "speech")
 public class Speech extends BaseEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
