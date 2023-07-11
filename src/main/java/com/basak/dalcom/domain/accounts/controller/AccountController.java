db02b25c1f076aed220df74574fc0578dc1b873a
pcjs156 <qkrdbqls1001@gmail.com>
2023-07-11 21:08:36 +0900

    docs(user): 회원가입 API 문서화

--- src/main/java/com/basak/dalcom/domain/accounts/controller/AccountController.java
+++ src/main/java/com/basak/dalcom/domain/accounts/controller/AccountController.java
@@ -7,6 +7,11 @@ import com.basak.dalcom.domain.accounts.data.Account;
 import com.basak.dalcom.domain.accounts.service.AccountService;
 import com.basak.dalcom.domain.common.controllers.exceptions.FieldConflictResponseException;
 import com.basak.dalcom.domain.common.service.exceptions.DuplicatedFieldException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import javax.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,22 +22,25 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "accounts", description = "회원 관련 API")
 @AllArgsConstructor
 @RestController
 @Validated
-@RequestMapping("/accounts")
+@RequestMapping("/api/v1/accounts")
 public class AccountController {
 
     private final AccountService accountService;
 
-    /**
-     * 일반 사용자에 대한 회원 가입을 수행하는 API
-     *
-     * @param dto 회원 가입에 필요한 정보를 담은 DTO
-     * @return (201) 가입된 회원의 정보 (uuid)
-     * @return (400) Validating 실패
-     * @return (409) 이미 사용중인 email이나 phoneNumber로 요청된 경우
-     */
+    @Operation(
+        summary = "일반 사용자 회원 가입 API",
+        description = "일반 사용자에 대한 회원 가입을 수행하는 API"
+    )
+    @ApiResponse(responseCode = "201", description = "회원 가입 성공",
+        content = @Content(schema = @Schema(implementation = SignupSuccessDto.class)))
+    @ApiResponse(responseCode = "400", description = "Validation 실패",
+        content = @Content(schema = @Schema(implementation = Void.class)))
+    @ApiResponse(responseCode = "409", description = "이미 사용중인 email이나 phoneNumber인 경우",
+        content = @Content(schema = @Schema(implementation = Void.class)))
     @PostMapping("/user/signup")
     public ResponseEntity<SignupSuccessDto> userSignup(@Valid @RequestBody UserSignupDto dto)
         throws FieldConflictResponseException {
