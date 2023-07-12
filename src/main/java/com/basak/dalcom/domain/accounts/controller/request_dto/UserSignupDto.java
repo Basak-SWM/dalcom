38b7bb85bcf4afd1ec2d97c6c4251722aae8f03b
pcjs156 <qkrdbqls1001@gmail.com>
2023-07-12 10:27:54 +0900

    fix(accounts): 일반 회원 가입 dto가 deserialize 되지 않는 문제 해결

--- src/main/java/com/basak/dalcom/domain/accounts/controller/request_dto/UserSignupDto.java
+++ src/main/java/com/basak/dalcom/domain/accounts/controller/request_dto/UserSignupDto.java
@@ -6,8 +6,10 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Getter
 public class UserSignupDto {
