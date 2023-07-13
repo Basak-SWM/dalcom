c430b45da6128d36c5bd8a31e85aaced3664a36e
pcjs156 <qkrdbqls1001@gmail.com>
2023-07-13 16:12:47 +0900

    hotfix: AudioSegment Entity 복구

---
+++ src/main/java/com/basak/dalcom/domain/core/audio_segment/data/AudioSegment.java
@@ -0,0 +1,36 @@
+package com.basak.dalcom.domain.core.audio_segment.data;
+
+import com.basak.dalcom.domain.common.BaseEntity;
+import com.basak.dalcom.domain.core.speech.data.Speech;
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+@Entity
+@Table(name = "audio_segment")
+public class AudioSegment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "speech_id", nullable = false)
+    private Speech speech;
+
+    @Column(nullable = false)
+    private String fullAudioS3Url;
+
+    @Column(nullable = false)
+    private LocalDateTime uploadedAt;
+
+    @Transient
+    private LocalDateTime lastModifiedDate;
+}
