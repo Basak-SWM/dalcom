1eb9fce1dfed67de2e622c85cc64842115276758
pcjs156 <qkrdbqls1001@gmail.com>
2023-07-24 20:35:25 +0900

    build: Add sentry handler for staging env

--- src/main/java/com/basak/dalcom/domain/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/basak/dalcom/domain/common/exception/GlobalExceptionHandler.java
@@ -1,19 +1,27 @@
 package com.basak.dalcom.domain.common.exception;
 
+import io.sentry.Sentry;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@AllArgsConstructor
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
 
+    private final Environment environment;
+
     @ExceptionHandler(value = {Exception.class})
     public ResponseEntity<?> unhandledException(Exception ex) {
+        catchAsSentry(ex);
         for (StackTraceElement el : ex.getStackTrace()) {
             log.error(el.toString());
         }
@@ -22,6 +30,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = {HandledException.class})
     public ResponseEntity<?> handledException(HandledException ex) {
+        catchAsSentry(ex);
         Map<String, Object> body = new HashMap<>();
         body.put("message", ex.getReason());
 
@@ -30,5 +39,11 @@ public class GlobalExceptionHandler {
         }
 
         return new ResponseEntity<>(body, ex.getStatus());
+    }
+
+    private void catchAsSentry(Exception ex) {
+        if (Arrays.asList(environment.getActiveProfiles()).contains("staging")) {
+            Sentry.captureException(ex);
+        }
     }
 }
