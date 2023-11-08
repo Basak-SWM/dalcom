f7a7ed6938db779027247f8762d3b613326f2545
pcjs156 <qkrdbqls1001@gmail.com>
2023-11-08 17:19:32 +0900

    rollback: Add cross origin to account controller

--- src/main/java/com/basak/dalcom/domain/accounts/controller/AccountController.java
+++ src/main/java/com/basak/dalcom/domain/accounts/controller/AccountController.java
@@ -24,7 +24,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -35,8 +34,6 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "accounts", description = "회원 관련 API")
 @AllArgsConstructor
 @RestController
-@CrossOrigin(origins = {"http://localhost:3000",
-    "http://localhost:8000"}, allowedHeaders = "*", allowCredentials = "true")
 @RequestMapping("/api/v1/accounts")
 public class AccountController {
 
