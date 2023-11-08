18a3989a646f78b3a77bf8fb4785fb8e54708dd7
pcjs156 <qkrdbqls1001@gmail.com>
2023-11-08 18:36:55 +0900

    hotfix: Add cookie for debug

--- src/main/java/com/basak/dalcom/spring/security/filter/SecurityAuthenticationFilter.java
+++ src/main/java/com/basak/dalcom/spring/security/filter/SecurityAuthenticationFilter.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -24,6 +25,16 @@ public class SecurityAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
         FilterChain filterChain) throws ServletException, IOException {
         HttpSession session = request.getSession(true);
+
+        ResponseCookie cookie = ResponseCookie.from("YUBIN", "STUPID")
+            .path("/")
+            .sameSite("None")
+            .httpOnly(false)
+            .secure(false)
+            .maxAge(60 * 60 * 24 * 30)
+            .build();
+
+        response.addHeader("Set-Cookie", cookie.toString());
 
         if (!session.isNew()) {
             String userId = (String) session.getAttribute("userId");
